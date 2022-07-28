e63a1f78632fd8a1bc3f94b32dc4f205f1e4afd7
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-07-28 14:59:53 -0300

    feat: Adição de lógica inicial e estilos iniciais

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// Styles
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS ToDo</span> @ 2022
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// Styles
+import styles from "./TaskList.module.css";
+
+// Interfaces
+import { ITask } from "../interfaces/ITask";
+
+interface Props {
+  taskList: ITask[];
+}
+
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (taskList.map((task) =>
+        <div key={task.id} className={styles.task}>
+          <div className={styles.details}>
+            <h4>{task.title}</h4>
+            <p>Dificuldade: {task.difficulty}</p>
+          </div>
+          <div className={styles.actions}>
+            <i className='bi bi-pencil'></i>
+            <i className='bi bi-trash'></i>
+          </div>
+        </div>
+      )) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
+  );
+};
+
+export default TaskList;
