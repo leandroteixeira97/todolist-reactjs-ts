612b3cc7d0b5594518eda8b2f1a531e8b8634d24
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-07-28 15:44:12 -0300

    feat: Finalização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 // Styles
 import styles from "./App.module.css";
@@ -16,10 +17,44 @@ import { ITask } from "./interfaces/ITask";
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id != id;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
 
   return (
     <div className="App">
+      <Modal children={<TaskForm btnText="Editar tarefa" taskList={taskList} task={taskToUpdate} handleUpdate={updateTask} />} />
       <Header />
       <main className={styles.main}>
         <div>
@@ -28,7 +63,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} />
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask} />
         </div>
       </main>
       <Footer />
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -11,24 +11,39 @@ interface Props {
   btnText: string;
   taskList: ITask[];
   setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
+  task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void;
 }
 
-const TaskForm = ({ btnText, taskList, setTaskList }: Props) => {
+const TaskForm = ({ btnText, taskList, setTaskList, task, handleUpdate }: Props) => {
 
   const [id, setId] = useState<number>(0);
   const [title, setTitle] = useState<string>("");
   const [difficulty, setDifficulty] = useState<number>(0);
 
+  useEffect(() => {
+    if (task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task]);
+
   const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
 
     e.preventDefault();
-    const id = Math.floor(Math.random() * 1000);
-    const newTask: ITask = { id, title, difficulty };
 
-    setTaskList!([...taskList, newTask]);
+    if (handleUpdate) {
+      handleUpdate(id, title, difficulty)
+    } else {
+      const id = Math.floor(Math.random() * 1000);
+      const newTask: ITask = { id, title, difficulty };
 
-    setTitle("");
-    setDifficulty(0);
+      setTaskList!([...taskList, newTask]);
+
+      setTitle("");
+      setDifficulty(0);
+    }
 
   };
 
@@ -45,7 +60,7 @@ const TaskForm = ({ btnText, taskList, setTaskList }: Props) => {
     <form className={styles.form} onSubmit={addTaskHandler}>
       <div className={styles.input_container}>
         <label htmlFor="title">Título:</label>
-        <input type="text" name='title' placeholder='Título da tarefa' onChange={handleChange} value={title} />
+        <input required type="text" name='title' placeholder='Título da tarefa' onChange={handleChange} value={title} />
       </div>
       <div className={styles.input_container}>
         <label htmlFor="difficulty">Dificuldade:</label>
